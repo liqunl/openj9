5118284cbfa6445e7056036bbeca709598431c1b
Andrew Craik <ajcraik@ca.ibm.com>
2019-02-20 15:52:13 -0500

    Relcaim marked assumptions from the RAT at GC Start
    Rather than cleaning all assumptions at the end of a GC we will now
    clean a limited number of assumptions at GC start (both scavenge and
    global) to amortize the clean-up cost over time and minimize pauses.
    
    Signed-off-by: Andrew Craik <ajcraik@ca.ibm.com>

--- runtime/compiler/control/HookedByTheJit.cpp
+++ runtime/compiler/control/HookedByTheJit.cpp
@@ -1477,6 +1477,7 @@ static void jitHookGlobalGCStart(J9HookInterface * * hookInterface, UDATA eventN
 
    if (jitConfig && jitConfig->runtimeFlags & J9JIT_GC_NOTIFY)
       printf("\n{GGC");
+   jitReclaimMarkedAssumptions(false);
    }
 
 static void jitHookLocalGCStart(J9HookInterface * * hookInterface, UDATA eventNum, void * eventData, void * userData)
@@ -1499,6 +1500,7 @@ static void jitHookLocalGCStart(J9HookInterface * * hookInterface, UDATA eventNu
       printf("\n<jit: enabling stack tracing at gc %d>", jitConfig->gcCount);
       TR::Options::getCmdLineOptions()->setVerboseOption(TR_VerboseGc);
       }
+   jitReclaimMarkedAssumptions(false);
    }
 
 static void jitHookGlobalGCEnd(J9HookInterface * * hookInterface, UDATA eventNum, void * eventData, void * userData)
@@ -6787,7 +6789,7 @@ static void jitHookReleaseCodeGlobalGCEnd(J9HookInterface **hook, UDATA eventNum
    MM_GlobalGCEndEvent *event = (MM_GlobalGCEndEvent *)eventData;
    J9VMThread  *vmThread  = (J9VMThread*)event->currentThread->_language_vmthread;
    jitReleaseCodeStackWalk(vmThread->omrVMThread);
-   jitReclaimMarkedAssumptions();
+   jitReclaimMarkedAssumptions(true);
    }
 
 static void jitHookReleaseCodeGCCycleEnd(J9HookInterface **hook, UDATA eventNum, void *eventData, void *userData)
@@ -6799,14 +6801,14 @@ static void jitHookReleaseCodeGCCycleEnd(J9HookInterface **hook, UDATA eventNum,
       condYield = event->condYieldFromGCFunction;
 
    jitReleaseCodeStackWalk(omrVMThread,condYield);
-   jitReclaimMarkedAssumptions();
+   jitReclaimMarkedAssumptions(true);
    }
 
 static void jitHookReleaseCodeLocalGCEnd(J9HookInterface **hook, UDATA eventNum, void *eventData, void *userData)
    {
    MM_LocalGCEndEvent *event = (MM_LocalGCEndEvent *)eventData;
    jitReleaseCodeStackWalk(event->currentThread);
-   jitReclaimMarkedAssumptions();
+   jitReclaimMarkedAssumptions(true);
    }
 
 
