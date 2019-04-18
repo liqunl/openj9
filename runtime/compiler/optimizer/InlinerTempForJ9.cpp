bd3c840f1f107d7bde0e33433563d9fd8beef76a
Yi Zhang <yizhang@ca.ibm.com>
2019-04-18 16:45:24 -0400

    remove inliner policy jsr292 restriction

--- runtime/compiler/optimizer/InlinerTempForJ9.cpp
+++ runtime/compiler/optimizer/InlinerTempForJ9.cpp
@@ -4741,7 +4741,7 @@ TR_InlinerFailureReason
       return static_cast<TR_InlinerFailureReason> (comp->fej9()->checkInlineableTarget(target, callsite));
 
    TR_ResolvedMethod * resolvedMethod = target->_calleeSymbol ? target->_calleeSymbol->getResolvedMethod():target->_calleeMethod;
-   if (isJSR292Method(resolvedMethod) &&
+   if (false && isJSR292Method(resolvedMethod) &&
       !isJSR292AlwaysInlineableMethod(resolvedMethod))
       return DontInline_Callee;
    else if (comp->ilGenRequest().details().isMethodHandleThunk() &&
