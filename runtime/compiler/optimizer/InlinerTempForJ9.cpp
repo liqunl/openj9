cfdb1e6efd56c54330335a6c34334453ff68dce6
Liqun Liu <liqunl@ca.ibm.com>
2019-05-02 13:24:25 -0400

    Skip HCR guard for CollectHandle
    Signed-off-by: Liqun Liu <liqunl@ca.ibm.com>

--- runtime/compiler/optimizer/InlinerTempForJ9.cpp
+++ runtime/compiler/optimizer/InlinerTempForJ9.cpp
@@ -2473,6 +2473,8 @@ TR_J9InlinerPolicy::skipHCRGuardForCallee(TR_ResolvedMethod *callee)
       return true;
    else if (length >= 30 && !strncmp(className, "java/lang/invoke/ConvertHandle", 30))
       return true;
+   else if (length >= 30 && !strncmp(className, "java/lang/invoke/CollectHandle", 30))
+      return true;
    else if (length >= 29 && !strncmp(className, "java/lang/invoke/AsTypeHandle", 29))
       return true;
    else if (length >= 36 && !strncmp(className, "java/lang/invoke/ArgumentMoverHandle", 36))
