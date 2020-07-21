ab6ccef1b91d6c7ad242c03a899f6ab954e0664f
Jack Lu <Jack.S.Lu@ibm.com>
2020-07-21 12:15:33 -0400

    update invokehandle bytecode to support OpenJDK MH
    Signed-off-by: Jack Lu <Jack.S.Lu@ibm.com>

--- runtime/vm/BytecodeInterpreter.hpp
+++ runtime/vm/BytecodeInterpreter.hpp
@@ -8137,6 +8137,35 @@ retry:
 		}
 done:
 		return rc;
+#elif defined(J9VM_OPT_OPENJDK_METHODHANDLE)
+retry:
+		VM_BytecodeAction rc = GOTO_RUN_METHOD;
+		U_16 index = *(U_16*)(_pc + 1);
+		J9ConstantPool *ramConstantPool = J9_CP_FROM_METHOD(_literals);
+		J9RAMMethodRef *ramMethodRef = ((J9RAMMethodRef*)ramConstantPool) + index;
+		UDATA methodTypeIndex = ramMethodRef->methodIndexAndArgCount >> 8;
+		j9object_t volatile type = J9_CLASS_FROM_CP(ramConstantPool)->methodTypes[methodTypeIndex];
+		j9object_t volatile memberNameObject = (j9object_t)ramMethodRef->method;
+		if (J9_EXPECTED(NULL != memberNameObject)) {
+			*--_sp = (UDATA)type;
+			_sendMethod = (J9Method *)J9OBJECT_ADDRESS_LOAD(_currentThread, memberNameObject, _vm->vmtargetOffset);
+		} else {
+			buildGenericSpecialStackFrame(REGISTER_ARGS, 0);
+			updateVMStruct(REGISTER_ARGS);
+			// add new resolve code which calls sendResolveInvokeHandle -> MHN.linkMethod()
+			// store the appendix value in methodTypes[MTindex] and memberName in slot2
+			resolveMethodHandle(_currentThread, ramConstantPool, index, J9_RESOLVE_FLAG_RUNTIME_RESOLVE);
+			VMStructHasBeenUpdated(REGISTER_ARGS);
+			restoreGenericSpecialStackFrame(REGISTER_ARGS);
+			if (immediateAsyncPending()) {
+				rc = GOTO_ASYNC_CHECK;
+			} else if (VM_VMHelpers::exceptionPending(_currentThread)) {
+				rc = GOTO_THROW_CURRENT_EXCEPTION;
+			} else {
+				goto retry;
+			}
+		}
+		return rc;
 #else
 	Assert_VM_unreachable();
 	return EXECUTE_BYTECODE;
